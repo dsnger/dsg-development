a24e6e2ca54c7449c345c9444530601b3fb3fa3e
Daniel <dsnger@arcor.de>
2017-08-12 14:57:01 +0200

    add bower_components, deleting was a mistake

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -5,12 +5,13 @@ $( document ).ready(function() {
   // Get started with all modules here
   console.log("App.js is ready")
 
-  //hamburger animation and function
+  /* hamburger animation and function as nav-overlay trigger */
   $('#hamburger__trigger-overlay').on('click', function(event){
     event.preventDefault();
     $('.nav-overlay').toggleClass('open close');
     $('.site-wrapper').toggleClass('nav-overlay--open nav-overlay--close');
     $(this).toggleClass('is-active');
   });
+
 
 });
